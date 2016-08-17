514d87e78e85375140e30b3bbf60bef61b7bb7a6
ZPC199012 <pengcheng199012zhao@gmail.com>
2016-08-17 16:17:40 -0400

    Able to Clear all Cells data
    Still need to adjust avoiding some cells from deleting

--- IP_NZ/frmService.cs
+++ IP_NZ/frmService.cs
@@ -55,6 +55,14 @@ namespace IP_NZ
 
         private void clearList_Click(object sender, EventArgs e)
         {
+            long i = 0;
+            //var oXL = new Microsoft.Office.Interop.Excel.Application();
+            Excel.Worksheet oWS = null;
+            long rMin = 0;
+            long rMax = 0;
+            Range rngClear = default(Range);
+
+
             Excel.Application excelApp = new Excel.Application();
 
             // if you want to make excel visible to user, set this property to true, false by default
@@ -69,87 +77,54 @@ namespace IP_NZ
             Excel.Sheets excelSheets = excelWorkbook.Worksheets;
 
             // get some sheet
-            string currentSheet = "Configuration";
+            string currentSheet = "Renewals";
             Excel.Worksheet excelWorksheet =
                 (Excel.Worksheet)excelSheets.get_Item(currentSheet);
 
             // access cell within sheet
             Excel.Range excelCell =
-                  (Excel.Range)excelWorksheet.get_Range("A1", "A1");
+                  (Excel.Range)excelWorksheet.get_Range("A5", "B25");
 
-            //var excelApp = new Excel.Application();
-            //excelApp.Workbooks.Add();
+            
+            try
+            {
+                //oWS = oXL.Worksheets["Renewals"]; //oXL.Parent.Worksheets.Item["myXlSheet"];
+                excelWorksheet.Activate();
 
-            //excelApp.ActiveCell.FormulaR1C1 = "1";
-            //excelApp.Range["A2"].Select();
-            //excelApp.ActiveCell.FormulaR1C1 = "2";
-            //excelApp.Range["A1:A2"].Select();
-            //excelApp.Selection.AutoFill(
-            //    Destination: excelApp.Range["A1:A10"],
-            //    Type: Excel.XlAutoFillType.xlFillDefault);
-            //excelApp.Range["A1:A10"].Select();
+                //var _with1 = oXL.Worksheets["Renewals"];
+                rMin = 5;
+                //First row of IP renewal data
 
-            //excelApp.Selection.Interior.Pattern = Excel.Constants.xlSolid;
-            //excelApp.Selection.Interior.PatternColorIndex = Excel.Constants.xlAutomatic;
-            //excelApp.Selection.Interior.ThemeColor = Excel.XlThemeColor.xlThemeColorAccent1;
-            //excelApp.Selection.Interior.TintAndShade = 0.399945066682943;
-            //excelApp.Selection.Interior.PatternTintAndShade = 0;
+                rMax = excelWorksheet.UsedRange.Rows.Count;
 
-            //excelApp.Range["A1:A10"].Select();
-            //excelApp.ActiveSheet.Shapes.AddChart.Select();
-            //excelApp.ActiveChart.ChartType = Excel.XlChartType.xlConeColStacked;
-            //excelApp.ActiveChart.SetSourceData(Source: excelApp.Range["Sheet1!$A$1:$A$10"]);
+                for (i = rMin; i <= rMax; i++)
+                {
+                    //Excel.Range cel = excelWorksheet.Cells[6, 2];
+                    //cel.Delete();
 
-            //excelApp.Visible = true;
+                    //excelWorksheet.Range("A5", "B10");
+                    excelWorksheet.Rows.Delete(i);
+                }
 
-            //long rMin = 0;
-            //long rMax = 0;
-            //Range rngClear = default(Range);
+                //rMax = oWS.Range("A" + oWS.Rows.Count).End(-4162).Row;
+                //Last row of IP renewal data
+                //if (rMax >= 5)
+                //    oWS.Range(oWS.Rows(rMin), oWS.Rows(rMax)).Delete();
 
+            }
 
+            catch (Exception ex)
+            {
+                Console.Write(ex.Message);
 
+            }
 
-            //long i = 0;
-            //var oXL = new Microsoft.Office.Interop.Excel.Application();
-            //Excel.Worksheet oWS = null;
-            //long rMin = 0;
-            //long rMax = 0;
-            //Range rngClear = default(Range);
-
-            //try
-            //{
-            //    oWS = oXL.Worksheets["Renewals"]; //oXL.Parent.Worksheets.Item["myXlSheet"];
-            //    oWS.Activate();
-
-            //    //var _with1 = oXL.Worksheets["Renewals"];
-            //    rMin = 5;
-            //    //First row of IP renewal data
-
-            //    rMax = oWS.UsedRange.Rows.Count;
-            //    for (i = rMin; i <= rMax; i++)
-            //    {
-            //        oWS.Rows.Delete(i);
-            //    }
-
-            //    //rMax = oWS.Range("A" + oWS.Rows.Count).End(-4162).Row;
-            //    //Last row of IP renewal data
-            //    //if (rMax >= 5)
-            //    //    oWS.Range(oWS.Rows(rMin), oWS.Rows(rMax)).Delete();
-
-            //}
-
-            //catch (Exception ex)
-            //{
-            //    Console.Write(ex.Message);
-
-            //}
-
-            //finally
-            //{
-            //    //oWS.Close();
-            //    oXL.Quit();
-            //    ResetCurrentCulture();
-            //}
+            finally
+            {
+                //oWS.Close();
+                excelApp.Quit();
+                //ResetCurrentCulture();
+            }
 
         }
     }
